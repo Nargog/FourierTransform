ea9a9d8b9161d75ee3bd5a40963d374f58353b60
Mats Hammarqvist <mats.hammarqvist@icloud.com>
2018-03-03 14:46:34 +0100

    Initial Commit

---
+++ FT_simple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FT_simple
+//
+//  Created by Mats Hammarqvist on 2018-03-03.
+//  Copyright © 2018 Mats Hammarqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
